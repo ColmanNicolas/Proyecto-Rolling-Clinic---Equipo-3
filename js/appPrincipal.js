5f06dd46555d6fb84380fcd5f1354576cd1d02bf
cristiancosti <cristtiancm@gmail.com>
2023-11-26 09:17:16 -0300

    agrego funciones para guardar en LS las listas de pacientes y medicos

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -14,9 +14,66 @@ function actualizarContenidoArrayLS(arreglo, listaLS) {
 function limpiarYenfocarPrimerImput(idElemento, valorImput) {
   //funcion para limpiar el formulario y seleccionar el primer imput
   document.getElementById(idElemento).reset();
-  const primerCampo = document.querySelector(
-    'input[type="' + valorImput + '"]'
-  );
+  const primerCampo = document.querySelector('input[type="' + valorImput + '"]');
   primerCampo.focus();
 }
 
+let pacientes = [];
+function manejarFormPaciente(event) {
+  event.preventDefault();
+  let nombrePaciente = document.getElementById("nombrePaciente").value;
+  let apellidoPaciente = document.getElementById("apellidoPaciente").value;
+  let dniPaciente = document.getElementById("dniPaciente").value;
+  let edadPaciente = document.getElementById("edadPaciente").value;
+  let emailPaciente = document.getElementById("emailPaciente").value;
+  let contrasenaPaciente = document.getElementById("contrasenaPaciente").value;
+  let repetirContrasenaPaciente = document.getElementById("repetirContrasenaPaciente").value;
+
+  const paciente = {
+    nombrePaciente,
+    apellidoPaciente,
+    dniPaciente,
+    edadPaciente,
+    emailPaciente,
+    contrasenaPaciente,
+    repetirContrasenaPaciente,
+  };
+
+  pacientes.push(paciente);
+
+  localStorage.setItem("listaPacientes", JSON.stringify(pacientes));
+}
+
+let medicos = [];
+function manejarFormMedico(event) {
+  console.log(event);
+
+  event.preventDefault();
+
+  let nombreMedico = document.getElementById("nombreMedico").value;
+  let apellidoMedico = document.getElementById("apellidoMedico").value;
+  let dniMedico = document.getElementById("dniMedico").value;
+  let matricula = document.getElementById("matricula").value;
+  let especialidad = document.getElementById("especialidad").value;
+  let centroMedico = document.getElementById("centroMedico").value;
+  let emailMedico = document.getElementById("emailMedico").value;
+  let contrasenaMedico = document.getElementById("contrasenaMedico").value;
+  let repetirContrasenaMedico = document.getElementById("repetirContrasenaMedico").value;
+
+  const medico = {
+    nombreMedico,
+    apellidoMedico,
+    dniMedico,
+    matricula,
+    matricula,
+    especialidad,
+    centroMedico,
+    emailMedico,
+    contrasenaMedico,
+    repetirContrasenaMedico,
+  };
+
+  medicos.push(medico);
+
+  localStorage.setItem("listaMedicos", JSON.stringify(medicos));
+}
